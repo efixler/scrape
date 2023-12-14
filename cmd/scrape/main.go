22d8b2dcb8fd5f582043c1b9751f9080b974de34
Eric Fixler <efixler@mozilla.com>
2023-12-14 13:51:57 -0500

    Now backed by storage

--- cmd/scrape/main.go
+++ cmd/scrape/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -11,6 +12,8 @@ import (
 	"time"
 
 	"github.com/efixler/scrape/resource"
+	"github.com/efixler/scrape/store"
+	"github.com/efixler/scrape/store/sqlite"
 	_ "github.com/go-shiori/go-readability"
 	_ "github.com/markusmobius/go-domdistiller"
 	"github.com/markusmobius/go-trafilatura"
@@ -19,21 +22,33 @@ import (
 var (
 	httpClient = &http.Client{Timeout: 30 * time.Second}
 	flags      flag.FlagSet
-	content    bool
+	// content_only bool
+	noContent bool
 )
 
-func main() {
-	url := flags.Arg(0)
+func fetch(url string) (*resource.WebPage, error) {
+	// change this interface to work through hiher level store.
+	db, err := sqlite.Open(context.TODO(), "./scrape_data/scrape.db")
+	if err != nil {
+		return nil, err
+	}
 	parsedUrl, err := nurl.Parse(url)
 	if err != nil {
-		fmt.Printf("Error: %s\n", err)
-		usage()
+		return nil, err
 	}
+	item, err := db.Fetch(parsedUrl)
+	if err != nil {
+		return nil, err
+	}
+	if item != nil {
+		return &item.Data, nil
+	}
+	// if we get here we're not cached
+
 	response, err := httpClient.Get(url)
 	if err != nil {
-		log.Fatalf("Could not get url: %s", err)
+		return nil, err
 	}
-	fmt.Printf("url: %s\n", parsedUrl)
 	defer response.Body.Close()
 	topts := trafilatura.Options{
 		IncludeImages:      true,
@@ -42,18 +57,44 @@ func main() {
 	}
 	result, err := trafilatura.Extract(response.Body, topts)
 	if err != nil {
-		log.Fatalf("failed to extract: %v", err)
-	}
-	if content {
-		fmt.Println(result.ContentText)
-		return
+		return nil, err
 	}
 	resource := &resource.WebPage{
 		Metadata:    result.Metadata,
 		ContentText: result.ContentText,
 		ParsedUrl:   parsedUrl,
 	}
-	marshaled, err := json.MarshalIndent(resource, "", "  ")
+	// this is annoying and dumb
+	sd := &store.StoredUrlData{
+		Data: *resource,
+	}
+	// need to make sure the app quit waits for the go func to finish
+	// go func() {
+	// 	// possibly need a better way to do this, or at least to capture errors
+	// 	//
+	_, err = db.Store(sd)
+	if err != nil {
+		log.Printf("Error storing %s: %s", url, err)
+	}
+	// }()
+
+	return resource, nil
+}
+
+func main() {
+	url := flags.Arg(0)
+	parsedUrl, err := nurl.Parse(url)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		usage()
+	}
+
+	// maybe filter utm_sources here
+	page, err := fetch(parsedUrl.String())
+	if err != nil {
+		log.Fatalf("Error fetching %s: %s", url, err)
+	}
+	marshaled, err := json.MarshalIndent(page, "", "  ")
 	//marshaled, err := result.Metadata.MarshalText(result.Metadata)
 	if err != nil {
 		log.Fatalf("failed to marshal: %v", err)
@@ -62,8 +103,8 @@ func main() {
 }
 
 func init() {
-	flags.Init("", flag.ExitOnError)
-	flags.BoolVar(&content, "t", false, "Get text content only")
+	flags.Init("xyz", flag.ExitOnError)
+	flags.BoolVar(&noContent, "T", false, "Skip text content")
 	// flags automatically adds -h and --help
 	flags.Usage = usage
 	flags.Parse(os.Args[1:])
@@ -82,7 +123,6 @@ func usage() {
   -p    Prune local storage and exit
   -P    Remove all stored entries from the cache
   -h	Show this help message
-  -t	Get text content only
   -T 	Don't get text content`)
 
 	flags.PrintDefaults()
