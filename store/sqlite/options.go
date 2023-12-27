2eaed357930fa326a803f18424ad8401ffbe9f3e
Eric Fixler <efixler@mozilla.com>
2023-12-26 23:25:26 -0500

    - Fix DB timeout - Type sqlite db string options - Really commit const name changes

--- store/sqlite/options.go
+++ store/sqlite/options.go
@@ -5,27 +5,29 @@ import (
 	"time"
 )
 
+type WALMode string
+type SyncMode string
+
 const (
-	DEFAULT_BUSY_TIMEOUT = 5 * time.Second
-	DEFAULT_JOURNAL_MODE = "WAL"
-	DEFAULT_CACHE_SIZE   = 20000
-	smallCacheSize       = 2000 // This is actually the sqlite default
-	SQLITE_SYNC_OFF      = "OFF"
-	SQLITE_SYNC_NORMAL   = "NORMAL"
-	DEFAULT_SYNC         = SQLITE_SYNC_OFF
+	FiveSecondDuration          = 5 * time.Second
+	WALJournalMode     WALMode  = "WAL"
+	BigCacheSize                = 20000
+	NormalCacheSize             = 2000 // This is actually the sqlite default
+	SyncOff            SyncMode = "OFF"
+	SyncNormal         SyncMode = "NORMAL"
 )
 
 type sqliteOptions struct {
 	busyTimeout time.Duration
-	journalMode string
+	journalMode WALMode
 	cacheSize   int
-	synchronous string
+	synchronous SyncMode
 }
 
 func (o sqliteOptions) String() string {
 	return fmt.Sprintf(
 		"_busy_timeout=%d&_journal_mode=%s&_cache_size=%d&_sync=%s",
-		o.busyTimeout,
+		o.busyTimeout.Milliseconds(),
 		o.journalMode,
 		o.cacheSize,
 		o.synchronous,
@@ -35,19 +37,19 @@ func (o sqliteOptions) String() string {
 // Returns an options set tuned for on-disk databases
 func DefaultOptions() sqliteOptions {
 	return sqliteOptions{
-		busyTimeout: DEFAULT_BUSY_TIMEOUT,
-		journalMode: DEFAULT_JOURNAL_MODE,
-		cacheSize:   DEFAULT_CACHE_SIZE,
-		synchronous: DEFAULT_SYNC,
+		busyTimeout: FiveSecondDuration,
+		journalMode: WALJournalMode,
+		cacheSize:   BigCacheSize,
+		synchronous: SyncOff,
 	}
 }
 
 // Returns an options set tuned for in-memory databases
 func InMemoryOptions() sqliteOptions {
 	return sqliteOptions{
-		busyTimeout: DEFAULT_BUSY_TIMEOUT,
-		journalMode: DEFAULT_JOURNAL_MODE,
-		cacheSize:   smallCacheSize,
-		synchronous: SQLITE_SYNC_NORMAL,
+		busyTimeout: FiveSecondDuration,
+		journalMode: WALJournalMode,
+		cacheSize:   NormalCacheSize,
+		synchronous: SyncNormal,
 	}
 }
